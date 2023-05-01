7fd921080a63448122c7286321a9fc828d2b744c
Patryk <sucheckipatryk@gmail.com>
2023-05-01 13:31:32 -0400

    Lab completed. 9 passing.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetValue){
+  if (feetValue <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feetValue >= 400 && feetValue <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feetValue >=2001 && feetValue <= 2499) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feetValue >= 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityValue){
+  return cityValue === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipValue){
+  switch(tipValue) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
